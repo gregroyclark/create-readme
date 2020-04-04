a97b4e5f95bac9f2d25d5a80a6805e705b9fc64d
peacefrog <gregroyclark@gmail.com>
2020-04-04 13:36:15 -0500

    adds semicolon in line 8 api.js

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ const api = {
     getUser(userName){
         axios
         .get(`https://api.github.com/users/${username}`, {
-            // headers: {"Authorization" `token ${process.env.GH_TOKEN}`}
+            headers: {"Authorization": `token ${process.env.GH_TOKEN}`}
         })
         .then(response => console.log(response.data))
         .catch(error => console.log(error))
